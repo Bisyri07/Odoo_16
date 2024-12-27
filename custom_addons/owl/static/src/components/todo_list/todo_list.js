429c792cfe95329cfc4cab049b6aeba40571b812
bisyri 08 <mbisyri33@gmail.com>
2024-12-27 20:27:05 +0700

    Initial setup for CRUD operation

--- custom_addons/owl/static/src/components/todo_list/todo_list.js
+++ custom_addons/owl/static/src/components/todo_list/todo_list.js
@@ -8,19 +8,37 @@ import { useService } from '@web/core/utils/hooks';
 export class OwlTodoList extends Component {
     setup(){
         this.state = useState({
-            taskList: []
+            // initial state
+            task: {name:'', completed:false, color:'#ff0000'},
+            taskList: [],
+
+            // custom properties
+            isEdit: false,
+            activeId: false,
         })
 
-        this.orm = useService("orm")
-        this.model = "owl.todo.list"
+        this.orm = useService('orm')
+        this.model = 'owl.todo.list'
 
         onWillStart(async ()=> {
             await this.getAllTask()
         })
-    }
+    };
 
     async getAllTask(){
         this.state.taskList = await this.orm.searchRead(this.model, [], ['name', 'completed', 'color']) 
+    };
+
+    addTask(){
+
+    };
+
+    editTask(){
+
+    };
+
+    async saveTask(){
+        console.log('save task', this.state.task)
     }
 
 };
