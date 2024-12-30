e0ffbd12f51ec4c2de333e752e4382432aedad98
bisyri 08 <mbisyri33@gmail.com>
2024-12-30 20:43:48 +0700

    Fetch grouped customer locations (state_id) from the res.partner model using readGroup.

--- custom_addons/owl/static/src/components/view_inheritance/res_partner_kanban_view.js
+++ custom_addons/owl/static/src/components/view_inheritance/res_partner_kanban_view.js
@@ -5,12 +5,23 @@ import { kanbanView } from '@web/views/kanban/kanban_view';
 import { KanbanController } from '@web/views/kanban/kanban_controller';
 import { useService } from '@web/core/utils/hooks';
 
+const { onWillStart } = owl;
+
 
 class ResPartnerKanbanController extends KanbanController {
     setup(){
         super.setup()
-        console.log("This is res partner kanban controller")
+        // console.log("This is res partner kanban controller")
         this.action = useService("action")
+        this.orm = useService("orm")
+
+        onWillStart(async ()=>{
+            this.customerLocations = await this.orm.readGroup(
+              //(model, domain, fields, groupby, kwargs = {})
+                "res.partner", [], ['state_id'], ['state_id']
+            )
+            console.log(this.customerLocations)
+        })
     }
 
     openSalesView(){
