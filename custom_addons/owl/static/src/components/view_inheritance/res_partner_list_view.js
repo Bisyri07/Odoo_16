3e5fb87e85c29f66da69b1be50a69ac16d95a58e
bisyri 08 <mbisyri33@gmail.com>
2024-12-30 12:01:45 +0700

    openSalesView method for View Sales Orders button

--- custom_addons/owl/static/src/components/view_inheritance/res_partner_list_view.js
+++ custom_addons/owl/static/src/components/view_inheritance/res_partner_list_view.js
@@ -3,13 +3,27 @@
 import { registry } from '@web/core/registry';
 import { listView } from '@web/views/list/list_view';
 import { ListController } from '@web/views/list/list_controller';
+import { useService } from '@web/core/utils/hooks';
 
 
 class resPartnerListController extends ListController {
     setup(){
         super.setup()
         console.log("This is res partner controller")
+        this.action = useService("action")
     }
+
+    openSalesView(){
+        console.log("open sales view works!")
+        // Open a Form View
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Customer Sales",
+            res_model: "sale.order",
+            views: [[false, "list"], [false, "form"]],    
+        })
+    }
+
 };
 
 export const resPartnerListView = {
